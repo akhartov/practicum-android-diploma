2789491594c2b4a7437083c1db91092d42c0a66f
a.gaifullina <nastya-25-97@mail.ru>
2026-02-21 18:27:18 +0700

    #1 добавлены библиотеки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,12 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
+    alias(libs.plugins.google.kspPlugin)
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+    id("org.jetbrains.kotlin.plugin.parcelize")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -27,25 +32,61 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
 
     buildFeatures {
         buildConfig = true
+        compose = true
+    }
+
+    ksp {
+        arg("room.schemaLocation", "$projectDir/schemas")
     }
 }
 
 dependencies {
     implementation(libs.androidX.core)
     implementation(libs.androidX.appCompat)
+    implementation(libs.converter.gson)
+    implementation(libs.gson.gson)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.glide)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.livedata)
+    implementation(libs.coil)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.koin)
+
+    annotationProcessor(libs.glidecompiler)
+
+    annotationProcessor(libs.room.compiler)
+    debugImplementation(libs.androidx.ui.tooling)
+    ksp(libs.room.compiler)
 
     // UI layer libraries
     implementation(libs.ui.material)
     implementation(libs.ui.constraintLayout)
+    implementation(libs.androidx.activity)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.fragment.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui)
+    implementation(libs.material3)
+    implementation(libs.androidx.runtime.livedata)
 
     // region Unit tests
     testImplementation(libs.unitTests.junit)
