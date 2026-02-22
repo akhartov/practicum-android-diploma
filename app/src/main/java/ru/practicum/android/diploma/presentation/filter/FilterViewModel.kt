87808b24ac62d6cef80aacefa3a8d4880017ff37
Aleksandr Nomokonov <aleks.agn@gmail.com>
2026-02-22 14:20:31 +0900

    Изменена инициализация ViewModels

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/FilterViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/FilterViewModel.kt
@@ -2,6 +2,4 @@ package ru.practicum.android.diploma.presentation.filter
 
 import androidx.lifecycle.ViewModel
 
-class FilterViewModel() : ViewModel() {
-
-}
+class FilterViewModel : ViewModel()
--- app/src/main/java/ru/practicum/android/diploma/presentation/team/TeamViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/team/TeamViewModel.kt
@@ -2,6 +2,4 @@ package ru.practicum.android.diploma.presentation.team
 
 import androidx.lifecycle.ViewModel
 
-class TeamViewModel() : ViewModel() {
-
-}
+class TeamViewModel : ViewModel()
