3e667fdc9fa300e284f5b70587e5d14a3e827f47
a.gaifullina <nastya-25-97@mail.ru>
2026-02-23 00:01:57 +0700

    xml to kt

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Dimens.kt
@@ -0,0 +1,29 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    // Падинги
+    val padding2 = 2.dp
+    val padding4 = 4.dp
+    val padding6 = 6.dp
+    val padding8 = 8.dp
+    val padding16 = 16.dp
+    val padding20 = 20.dp
+    val padding24 = 24.dp
+
+    // Углы
+    val cornerRadius12 = 12.dp
+
+    // Высоты элементов
+    val searchBarHeight = 56.dp
+    val buttonHeight = 60.dp
+    val salaryBarHeight = 56.dp
+    val toolBarHeight = 56.dp
+    val navigationBarHeight = 58.dp
+    val companyBarHeight = 58.dp
+    val industryItemHeight = 60.dp
+    val itemHeight = 60.dp
+
+
+}
