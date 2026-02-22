414f6f6e2cc78e51dd0de5e5cb6531e76c88a03b
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-02-22 10:19:01 +0300

    #12 plurals год

--- app/src/main/java/ru/practicum/android/diploma/util/WordDeclension.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/WordDeclension.kt
@@ -7,4 +7,8 @@ class WordDeclension(val context: Context) {
     fun getFoundVacanciesString(count: Int): String {
         return context.resources.getQuantityString(R.plurals.found_n_vacancies, count, count)
     }
+
+    fun getYears(count: Int): String {
+        return context.resources.getQuantityString(R.plurals.year_n, count, count)
+    }
 }
