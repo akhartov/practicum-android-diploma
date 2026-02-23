4d10796dc304e7ec844e3195b9cf8b3309841c0c
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-02-23 10:51:37 +0300

    Инициализация БД в модуле

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,21 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.db.AppDatabase
+import ru.practicum.android.diploma.data.db.dao.VacancyDao
 
 val dataModule = module {
+
+    single<AppDatabase> {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "app.db"
+        ).build()
+    }
+
+    single<VacancyDao> { get<AppDatabase>().vacancyDao() }
 
 }
