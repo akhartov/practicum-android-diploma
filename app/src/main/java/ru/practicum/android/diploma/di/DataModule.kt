598f7d4a5abcad1c994ec2c1aed8b4a6c4b14af2
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-02-23 11:17:56 +0300

    Инициализация VacancyApi в модуле

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,7 +1,38 @@
 package ru.practicum.android.diploma.di
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.data.network.VacancyApi
+import ru.practicum.android.diploma.data.network.VacancyApiClient
+import ru.practicum.android.diploma.data.network.VacancyApiClientImpl
 
 val dataModule = module {
 
+    single<VacancyApi> {
+        val headerInterceptor = Interceptor { chain ->
+            val originalRequest = chain.request()
+            val modifiedRequest = originalRequest.newBuilder()
+                .header("Authorization", "Bearer ${BuildConfig.API_ACCESS_TOKEN}")
+                .header("Content-Type", "application/json")
+                .build()
+            chain.proceed(modifiedRequest)
+        }
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(headerInterceptor)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(VacancyApi.HOST_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(VacancyApi::class.java)
+    }
+
+    factory<VacancyApiClient> { VacancyApiClientImpl(get()) }
 }
