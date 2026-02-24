25dd1debcda741e8cddbd36b1199d5164c977166
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-02-24 20:06:55 +0300

    Стейты для экранов Поиск вакансий и Вакансия

--- app/src/main/java/ru/practicum/android/diploma/presentation/model/VacancyShortDetails.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/model/VacancyShortDetails.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.presentation.model
 
 data class VacancyShortDetails(
-    val vacancyTitle: String, // "${vacancy.name}, {employer.city}"
-    val employerName: String? = null, // Работодатель название
-    val employerLogoUrl: String? = null, // Работодатель логотип
-    val salaryString: String? = null, // "от $salaryFrom до $salary $salaryCurrencySign"
+    val vacancyTitle: String?, // "${VacancyDetailDto.name}, {AddressDto.city}"
+    val employerName: String? = null, // Работодатель название EmployerDto.name
+    val employerLogoUrl: String? = null, // Работодатель логотип EmployerDto.logoUrl
+    val salaryString: String? = null, // "от ${SalaryDto.from} до ${SalaryDto.to} ${SalaryDto.currency}"
 )
