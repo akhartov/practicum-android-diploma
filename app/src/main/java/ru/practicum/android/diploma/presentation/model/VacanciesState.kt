d1568c7a77e84d48ff11b03aad8926b274c79b9b
Andrey <andrew.khartov@gmail.com>
2026-02-24 23:05:59 +0300

    #10 Общие модели состояний слоя UI (#20)
    * #10 Общие модели состояний слоя UI
    
    * Стейты для экранов Поиск вакансий и Вакансия
    
    ---------
    
    Co-authored-by: Хартов Андрей Александрович <khartov.a.al@sberbank.ru>

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/model/VacanciesState.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.presentation.model
+
+sealed interface VacanciesState {
+    object Empty : VacanciesState // Поиск еще не был выполнен, на экране человек с биноклем
+    object NotFound : VacanciesState // Не удалось получить список вакансий
+    object Loading : VacanciesState // Идет поиск вакансий
+    object NoInternet : VacanciesState // Нет интернета
+    object ServerError : VacanciesState // Ошибка сервера
+    class Content(val vacancies: VacancyShortDetails) : VacanciesState
+}
