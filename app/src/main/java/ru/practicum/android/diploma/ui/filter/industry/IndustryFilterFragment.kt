4dc90311c9b371c6083b9831ccc2676209252bab
Aleksandr Nomokonov <aleks.agn@gmail.com>
2026-02-25 11:31:58 +0900

    #8 Граф навигации на XML и заглушки экранов в виде Fragment с Compose (#21)
    * Добавлен граф навигации на XML, BottomNavigationView и заглушки экранов в виде Fragment с Compose внутри

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/industry/IndustryFilterFragment.kt
@@ -0,0 +1,36 @@
+package ru.practicum.android.diploma.ui.filter.industry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+
+class IndustryFilterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+        setContent {
+            IndustryFilterScreen()
+        }
+    }
+}
+
+@Composable
+fun IndustryFilterScreen() {
+    Column {
+        Text(stringResource(R.string.industry_selection))
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/settings/FilterSettingsFragment.kt
@@ -0,0 +1,56 @@
+package ru.practicum.android.diploma.ui.filter.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
+
+class FilterSettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+        setContent {
+            FilterSettingsScreen(
+                navigateToWorkplaceFilter = {
+                    findNavController().navigate(R.id.action_filterSettingsFragment_to_workplaceFilterFragment)
+                },
+                navigateToIndustryFilter = {
+                    findNavController().navigate(R.id.action_filterSettingsFragment_to_industryFilterFragment)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun FilterSettingsScreen(
+    navigateToWorkplaceFilter: () -> Unit,
+    navigateToIndustryFilter: () -> Unit
+) {
+    Column {
+        Text(stringResource(R.string.filter_settings))
+        Button(
+            onClick = navigateToWorkplaceFilter,
+            content = { Text(stringResource(R.string.workplace_selection)) },
+        )
+        Button(
+            onClick = navigateToIndustryFilter,
+            content = { Text(stringResource(R.string.industry_selection)) },
+        )
+    }
+}
