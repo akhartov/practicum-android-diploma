3b65ead1693d10156f38c1654691640436f0313b
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-02-27 18:40:29 +0300

    #36 #42 Мапинг VacancyDetailDto в VacancyEntity и VacancyDetails

--- app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/VacancyDetailDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/VacancyDetailDto.kt
@@ -3,11 +3,11 @@ package ru.practicum.android.diploma.data.dto.vacancy
 data class VacancyDetailDto(
     val id: String,
     val name: String,
-    val salary: SalaryDto?,
-    val address: AddressDto?,
-    val experience: ExperienceDto?,
-    val schedule: ScheduleDto?,
-    val employment: EmploymentDto?,
+    val salary: SalaryDto? = null,
+    val address: AddressDto? = null,
+    val experience: ExperienceDto? = null,
+    val schedule: ScheduleDto? = null,
+    val employment: EmploymentDto? = null,
     val contacts: ContactsDto? = null,
     val description: String? = null,
     val employer: EmployerDto? = null,
@@ -16,4 +16,3 @@ data class VacancyDetailDto(
     val url: String? = null,
     val industry: IndustryDto? = null,
 )
-
