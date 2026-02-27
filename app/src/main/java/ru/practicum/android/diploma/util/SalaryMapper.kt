53a49d82a0b83ccf0f38de08e607a0b3c2f9e637
RweShuN <131369096+ReShunchik@users.noreply.github.com>
2026-02-27 14:50:54 +0300

    #27 функция форматирования текста зарплаты (#47)
    * #27 добавил функцию форматирования текста зарплаты
    
    добавил инициализацию SalaryMapper и CurrencyMapper в koin
    
    * fix detekt
    
    * fix detekt
    
    * Remove .idea folder from version control
    
    ---------
    
    Co-authored-by: ReShunchik <ваv.gorshenin_2004@mail.ru>

---
+++ app/src/main/java/ru/practicum/android/diploma/util/SalaryMapper.kt
@@ -0,0 +1,52 @@
+package ru.practicum.android.diploma.util
+
+import android.content.Context
+import ru.practicum.android.diploma.R
+
+class SalaryMapper(
+    val context: Context,
+    val currencyMapper: CurrencyMapper
+) {
+    // все элементы соотвествуют классу SalaryDto
+    fun getSalaryInfo(
+        from: Int? = null,
+        to: Int? = null,
+        currency: String
+    ): String {
+        // нижняя граница зарплаты
+        val limitFrom = formatLimit(
+            context.getString(R.string.from),
+            from
+        )
+        // верхняя граница зарплаты
+        val limitTo = formatLimit(
+            context.getString(R.string.to),
+            to
+        )
+        val currencySymbol = currencyMapper.codeToSymbol(currency)
+        return if (limitTo.isEmpty() && limitFrom.isEmpty()) {
+            context.getString(R.string.no_salary)
+        } else if (limitTo.isEmpty()) {
+            SAMPLE_ONE_LIMIT.format(limitFrom, currencySymbol)
+        } else if (limitFrom.isEmpty()) {
+            SAMPLE_ONE_LIMIT.format(limitTo, currencySymbol)
+        } else {
+            SAMPLE_TWO_LIMIT.format(limitFrom, limitTo, currencySymbol)
+        }
+    }
+
+    // форматирование одной границы
+    private fun formatLimit(limit: String, number: Int?): String {
+        return if (number != null) {
+            limit + SAMPLE_NUMBER.format(number)
+        } else {
+            ""
+        }
+    }
+
+    companion object {
+        const val SAMPLE_NUMBER = " %d"
+        const val SAMPLE_ONE_LIMIT = "%s %s"
+        const val SAMPLE_TWO_LIMIT = "%s %s %s"
+    }
+}
