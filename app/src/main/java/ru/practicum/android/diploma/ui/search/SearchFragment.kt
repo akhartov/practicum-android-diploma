e6c06070895f92b5442673ed55a9038b91eba877
Aleksandr Nomokonov <aleks.agn@gmail.com>
2026-02-28 15:17:53 +0900

    Добавлена compose-функция поля поиска с TextField (используется в экранах "Поиск вакансий" и "Выбор региона")

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,19 +1,31 @@
 package ru.practicum.android.diploma.ui.search
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.ui.common.SearchTextField
+import ru.practicum.android.diploma.ui.theme.AndroidDiplomaTheme
+import ru.practicum.android.diploma.ui.theme.Dimens
 
 class SearchFragment : Fragment() {
 
@@ -25,32 +37,71 @@ class SearchFragment : Fragment() {
         setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
         setContent {
-            SearchScreen(
-                navigateToFilterSettings = {
-                    findNavController().navigate(R.id.action_searchFragment_to_filterSettingsFragment)
-                },
-                navigateToVacancy = {
-                    findNavController().navigate(R.id.action_searchFragment_to_vacancyFragment)
-                }
-            )
+            AndroidDiplomaTheme {
+                SearchScreenContent()
+            }
         }
+    }
+
+    @Composable
+    fun SearchScreenContent() {
+        var searchQuery by remember { mutableStateOf("") }
+
+        SearchScreen(
+            searchQuery,
+            onQueryChange = { newQuery ->
+                searchQuery = newQuery
+            },
+            onFilterIconClick = {
+                findNavController().navigate(R.id.action_searchFragment_to_filterSettingsFragment)
+            },
+            onVacancyItemClick = {
+                findNavController().navigate(R.id.action_searchFragment_to_vacancyFragment)
+            }
+        )
     }
 }
 
 @Composable
 fun SearchScreen(
-    navigateToFilterSettings: () -> Unit,
-    navigateToVacancy: () -> Unit
+    searchQuery: String,
+    onQueryChange: (String) -> Unit,
+    onFilterIconClick: () -> Unit,
+    onVacancyItemClick: () -> Unit
 ) {
-    Column {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = Dimens.padding16)
+    ){
         Text(stringResource(R.string.search_vacancies))
         Button(
-            onClick = navigateToFilterSettings,
+            onClick = onFilterIconClick,
             content = { Text(stringResource(R.string.filter_settings)) },
         )
         Button(
-            onClick = navigateToVacancy,
+            onClick = onVacancyItemClick,
             content = { Text(stringResource(R.string.vacancy)) },
+        )
+        SearchTextField(
+            searchQuery = searchQuery,
+        placeholder = stringResource(R.string.enter_search_query),
+        onQueryChange = onQueryChange,
+        )
+    }
+}
+
+@Preview(name = "Light", showBackground = true)
+@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun SearchScreenEmptyPreview() {
+    AndroidDiplomaTheme {
+        SearchScreen(
+            searchQuery = "",
+            onQueryChange = {},
+            onFilterIconClick = {},
+            onVacancyItemClick = {},
         )
     }
 }
