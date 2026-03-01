53cd83115a298760e02a4a200903e91abf81b1a0
Aleksandr Nomokonov <aleks.agn@gmail.com>
2026-03-01 23:34:39 +0900

    fix Modifier

--- app/src/main/java/ru/practicum/android/diploma/ui/common/VacancyItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/VacancyItem.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -58,7 +59,7 @@ fun VacancyItem(
             AsyncImage(
                 model = vacancy.employerLogoUrl,
                 contentDescription = null,
-                modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(Dimens.cornerRadius12)),
+                modifier = Modifier.wrapContentSize().clip(RoundedCornerShape(Dimens.cornerRadius12)),
                 placeholder = painterResource(R.drawable.placeholder_vacancy_logo),
                 error = painterResource(R.drawable.placeholder_vacancy_logo),
                 contentScale = ContentScale.Fit
