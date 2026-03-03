a455a9ae76b0b682de6c71f5ba5e4ec4f919f6e2
ReShunchik <v.gorshenin_2004@mail.ru>
2026-03-03 10:12:01 +0300

    fix detekt добавил FilterIcon.kt

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/FilterInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/FilterInteractorImpl.kt
@@ -1,22 +1,21 @@
 package ru.practicum.android.diploma.domain.impl
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 import ru.practicum.android.diploma.domain.api.FilterInteractor
 import ru.practicum.android.diploma.domain.models.Filters
-import ru.practicum.android.diploma.domain.models.VacancyShortResponse
 import ru.practicum.android.diploma.ui.filter.workplace.FilterIconType
-import ru.practicum.android.diploma.util.Resource
 
 class FilterInteractorImpl : FilterInteractor {
     override fun searchWithFilters(filters: Filters): HashMap<String, String> {
-        TODO("Not yet implemented")
+        return HashMap()
     }
 
     override fun searchWithoutFilters(): HashMap<String, String> {
-        TODO("Not yet implemented")
+        return HashMap()
     }
 
     override fun getFilterIconState(): Flow<FilterIconType> {
-        TODO("Not yet implemented")
+        return emptyFlow()
     }
 }
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/FilterIcon.kt
@@ -0,0 +1,71 @@
+package ru.practicum.android.diploma.ui.filter.workplace
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.domain.models.Filters
+import ru.practicum.android.diploma.ui.theme.Dimens
+import ru.practicum.android.diploma.ui.theme.FilterIconColors
+import ru.practicum.android.diploma.ui.theme.LocalAndroidDiplomaScheme
+
+@Composable
+fun FilterIcon(
+    filterIconType: FilterIconType,
+    onClick: () -> Unit
+) {
+    val filterIconColors = LocalAndroidDiplomaScheme.current.filterIcon
+    val filterIconConfig = filterIconType.getFilterColors(filterIconColors)
+    IconButton(onClick = onClick) {
+        Box(
+            modifier = Modifier
+                .size(Dimens.filterIconSize)
+                .clip(RoundedCornerShape(Dimens.cornerRadius4))
+                .background(filterIconConfig.background),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_filter_icon),
+                contentDescription = "",
+                tint = filterIconConfig.hint,
+            )
+        }
+    }
+}
+
+sealed class FilterIconType {
+
+    abstract fun getFilterColors(customFilterIcon: FilterIconColors): FilterIconConfig
+
+    class hasFilterIcon(val filters: Filters) : FilterIconType() {
+        override fun getFilterColors(customFilterIcon: FilterIconColors): FilterIconConfig {
+            return FilterIconConfig(
+                background = customFilterIcon.hasFilterIcon.background,
+                hint = customFilterIcon.hasFilterIcon.hint
+            )
+        }
+    }
+
+    object noFilterIcon : FilterIconType() {
+        override fun getFilterColors(customFilterIcon: FilterIconColors): FilterIconConfig {
+            return FilterIconConfig(
+                background = customFilterIcon.noFilterIcon.background,
+                hint = customFilterIcon.noFilterIcon.hint
+            )
+        }
+    }
+}
+
+data class FilterIconConfig(
+    val background: Color,
+    val hint: Color
+)
