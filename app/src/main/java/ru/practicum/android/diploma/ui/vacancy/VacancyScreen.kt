ac94aea7dbe7a58e112e4a2d2c44f56671671d53
a.gaifullina <nastya-25-97@mail.ru>
2026-03-04 08:17:28 +0700

    detekt

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyScreen.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.ui.vacancy
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,12 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -25,18 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.domain.models.Contacts
-import ru.practicum.android.diploma.domain.models.MailData
-import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.presentation.model.LikeButtonState
 import ru.practicum.android.diploma.presentation.model.VacancyDetailsState
 import ru.practicum.android.diploma.presentation.vacancy.VacancyViewModel
@@ -196,6 +183,7 @@ private fun ErrorNoInternet() {
         }
     }
 }
+
 @Composable
 private fun ToolBar(likeState: LikeButtonState, vacancyViewModel: VacancyViewModel, onBack: () -> Unit) {
     Row(
