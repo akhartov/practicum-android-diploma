5c422ec3b579fc151bc5adcaa250f653b89078cf
ReShunchik <v.gorshenin_2004@mail.ru>
2026-03-05 22:48:11 +0300

    SavedFiltersDto.kt и Filters.kt теперь имеют поля одинакового типа Добавил приписку Id к полям area и industry

--- app/src/main/java/ru/practicum/android/diploma/data/dto/SavedFiltersDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/SavedFiltersDto.kt
@@ -3,16 +3,16 @@ package ru.practicum.android.diploma.data.dto
 import ru.practicum.android.diploma.domain.models.Filters
 
 data class SavedFiltersDto(
-    val area: String,
-    val industry: String,
-    val salary: String,
-    val isIncludeSalary: String
+    val areaId: Int?,
+    val industryId: Int?,
+    val salary: Int?,
+    val isIncludeSalary: Boolean
 )
 
 fun SavedFiltersDto.toFilters(): Filters =
     Filters(
-        area = if (area.isEmpty()) null else area.toInt(),
-        industry = if (industry.isEmpty()) null else industry.toInt(),
-        salary = if (salary.isEmpty()) null else salary.toInt(),
-        isIncludeSalary = this.isIncludeSalary.toBoolean()
+        areaId = areaId,
+        industryId = industryId,
+        salary = salary,
+        isIncludeSalary = isIncludeSalary
     )
--- app/src/main/java/ru/practicum/android/diploma/domain/models/Filters.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Filters.kt
@@ -3,16 +3,16 @@ package ru.practicum.android.diploma.domain.models
 import ru.practicum.android.diploma.data.dto.SavedFiltersDto
 
 data class Filters(
-    val area: Int?,
-    val industry: Int?,
+    val areaId: Int?,
+    val industryId: Int?,
     val salary: Int?,
     val isIncludeSalary: Boolean
 )
 
 fun Filters.toSavedFilters(): SavedFiltersDto =
     SavedFiltersDto(
-        area = area?.toString() ?: "",
-        industry = industry?.toString() ?: "",
-        salary = salary?.toString() ?: "",
-        isIncludeSalary = isIncludeSalary.toString()
+        areaId = areaId,
+        industryId = industryId,
+        salary = salary,
+        isIncludeSalary = isIncludeSalary
     )
