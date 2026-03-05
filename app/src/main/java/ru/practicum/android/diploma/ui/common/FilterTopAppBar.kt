86658a0b455f7136d0dd46ac89f3d6072b58b5ef
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-03-05 22:27:34 +0300

    #78 Общий TopAppBar для фрагментов фильтров

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/FilterTopAppBar.kt
@@ -0,0 +1,56 @@
+package ru.practicum.android.diploma.ui.common
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.ui.theme.Dimens
+import ru.practicum.android.diploma.ui.theme.LocalAndroidDiplomaScheme
+import ru.practicum.android.diploma.ui.theme.LocalAndroidDiplomaTypography
+
+@Composable
+fun FilterTopAppBar(
+    title: String,
+    onBackClick: () -> Unit
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        contentColor = LocalAndroidDiplomaScheme.current.topBar.text
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(Dimens.searchBarHeight)
+                .padding(end = Dimens.padding8),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = { onBackClick.invoke() }) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_back),
+                    contentDescription = null,
+                    tint = LocalAndroidDiplomaScheme.current.topBar.icon
+                )
+            }
+            Spacer(modifier = Modifier.width(Dimens.padding16))
+            Text(
+                text = title,
+                modifier = Modifier.weight(1f),
+                maxLines = 1,
+                style = LocalAndroidDiplomaTypography.current.medium22
+            )
+        }
+    }
+}
