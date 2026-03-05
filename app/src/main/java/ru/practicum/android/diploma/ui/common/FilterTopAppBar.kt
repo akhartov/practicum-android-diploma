42478687a2bb934dc7d186d5bd2e133eb2308a0e
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-03-05 23:25:30 +0300

    #78 Общий TopAppBar для фрагментов фильтров

--- app/src/main/java/ru/practicum/android/diploma/ui/common/FilterTopAppBar.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/FilterTopAppBar.kt
@@ -1,21 +1,19 @@
 package ru.practicum.android.diploma.ui.common
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.ui.theme.Dimens
 import ru.practicum.android.diploma.ui.theme.LocalAndroidDiplomaScheme
@@ -44,11 +42,11 @@ fun FilterTopAppBar(
                     tint = LocalAndroidDiplomaScheme.current.topBar.icon
                 )
             }
-            Spacer(modifier = Modifier.width(Dimens.padding16))
             Text(
                 text = title,
                 modifier = Modifier.weight(1f),
                 maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 style = LocalAndroidDiplomaTypography.current.medium22
             )
         }
