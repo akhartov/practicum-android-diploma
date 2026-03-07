b399791d53de6cdf46919236408355daba4fdf52
Хартов Андрей Александрович <khartov.a.al@sberbank.ru>
2026-03-07 22:51:34 +0300

    Верстка и вьюмодель места работы #79

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/WorkplaceFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/WorkplaceFilterFragment.kt
@@ -4,19 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_NO
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.presentation.filter.settings.FilterSettingsViewModel
-import kotlin.getValue
+import ru.practicum.android.diploma.ui.common.ButtonControl
+import ru.practicum.android.diploma.ui.common.ButtonControlType
+import ru.practicum.android.diploma.ui.common.FilterSectionControlType
+import ru.practicum.android.diploma.ui.common.FilterSelectionControl
+import ru.practicum.android.diploma.ui.common.FilterTopAppBar
+import ru.practicum.android.diploma.ui.theme.AndroidDiplomaTheme
+import ru.practicum.android.diploma.ui.theme.Dimens
 
 class WorkplaceFilterFragment : Fragment() {
 
@@ -30,14 +42,21 @@ class WorkplaceFilterFragment : Fragment() {
         setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
 
         setContent {
-            WorkplaceFilterScreen(
-                navigateToCountryFilter = {
-                    findNavController().navigate(R.id.action_workplaceFilterFragment_to_countryFilterFragment)
-                },
-                navigateToRegionFilter = {
-                    findNavController().navigate(R.id.action_workplaceFilterFragment_to_regionFilterFragment)
-                }
-            )
+            AndroidDiplomaTheme {
+                WorkplaceFilterScreen(
+                    navigateToCountryFilter = {
+                        findNavController().navigate(R.id.action_workplaceFilterFragment_to_countryFilterFragment)
+                    },
+                    navigateToRegionFilter = {
+                        findNavController().navigate(R.id.action_workplaceFilterFragment_to_regionFilterFragment)
+                    },
+                    onApply = {
+                        filterSettingsViewModel.applyWorkplace()
+                        findNavController().popBackStack()
+                    },
+                    onBackClick = { findNavController().popBackStack() }
+                )
+            }
         }
     }
 }
@@ -45,17 +64,68 @@ class WorkplaceFilterFragment : Fragment() {
 @Composable
 fun WorkplaceFilterScreen(
     navigateToCountryFilter: () -> Unit,
-    navigateToRegionFilter: () -> Unit
+    navigateToRegionFilter: () -> Unit,
+    onApply: () -> Unit,
+    onBackClick: () -> Unit,
 ) {
-    Column {
-        Text(stringResource(R.string.workplace_selection))
-        Button(
-            onClick = navigateToCountryFilter,
-            content = { Text(stringResource(R.string.country_selection)) },
-        )
-        Button(
-            onClick = navigateToRegionFilter,
-            content = { Text(stringResource(R.string.region_selection)) },
+    Scaffold(
+        topBar = {
+            FilterTopAppBar(
+                title = stringResource(R.string.workplace_selection),
+                onBackClick = { onBackClick() }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(horizontal = Dimens.padding16),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column {
+                FilterSelectionControl(
+                    Modifier,
+                    onClick = { navigateToCountryFilter() },
+                    filterSectionControlType = FilterSectionControlType.Absent,
+                    text = stringResource(R.string.country_selection),
+                )
+                FilterSelectionControl(
+                    Modifier,
+                    onClick = { navigateToRegionFilter() },
+                    filterSectionControlType = FilterSectionControlType.Absent,
+                    text = stringResource(R.string.region_selection),
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(Dimens.padding8)
+            ) {
+                ButtonControl(
+                    Modifier,
+                    text = stringResource(R.string.select),
+                    onClick = { onApply() },
+                    buttonControlType = ButtonControlType.Approve,
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    device = "spec:width=1080px,height=1820px,dpi=420",
+    uiMode = UI_MODE_NIGHT_NO,
+    showBackground = true
+)
+@Composable
+fun PreviewScreenDay() {
+    AndroidDiplomaTheme {
+        WorkplaceFilterScreen(
+            {},
+            {},
+            {},
+            {},
         )
     }
 }
