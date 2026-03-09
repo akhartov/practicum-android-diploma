08e804a0f59997495248845e8d087292d4ddbdcd
ReShunchik <v.gorshenin_2004@mail.ru>
2026-03-09 10:00:39 +0300

    1. добавил индикатор прогресса при загрузке списка стран 2. все ошибки обрабатываються отдельно

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/country/CountryFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/country/CountryFilterFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
@@ -94,13 +98,36 @@ fun CountryFilterScreen(
                         }
                     }
 
-                    is CountryFilterState.Loading -> { }
-
-                    else -> {
+                    is CountryFilterState.NotFound -> {
                         PlaceholderState(
                             painter = painterResource(id = R.drawable.placeholder_region_error),
                             text = stringResource(id = R.string.region_selection_error)
                         )
+                    }
+
+                    is CountryFilterState.NoInternet -> {
+                        PlaceholderState(
+                            painter = painterResource(id = R.drawable.placeholder_region_error),
+                            text = stringResource(id = R.string.region_selection_error)
+                        )
+                    }
+
+                    is CountryFilterState.ServerError -> {
+                        PlaceholderState(
+                            painter = painterResource(id = R.drawable.placeholder_region_error),
+                            text = stringResource(id = R.string.region_selection_error)
+                        )
+                    }
+
+                    is CountryFilterState.Loading -> {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                Modifier.size(Dimens.progressIndicatorSize)
+                            )
+                        }
                     }
                 }
             }
